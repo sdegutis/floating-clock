f299b8f607c8f331e031c1fce8e9e2886a2733bc
sdegutis <sbdegutis@gmail.com>
2020-06-18 23:28:28 -0500

    v3

--- index.js
+++ index.js
@@ -1,43 +1,23 @@
-const ipc = require('electron').ipcRenderer;
+const [clock] = document.getElementsByTagName('div');
+const [closeButton] = document.getElementsByTagName('button');
 
-const clock = document.getElementById('clock');
-const closeButton = document.getElementById('closeButton');
+
+// Setup state
+
+let seconds = false;
+let size = 24;
+
+setupWindowsButtonColor();
+refreshClock();
+setInterval(refreshClock, 1000);
+fixClockSize();
+
+
+// Close button
+
+closeButton.hidden = true;
 
 closeButton.onclick = () => window.close();
-
-const color = new URLSearchParams(location.search).get('color');
-const style = document.createElement('style');
-style.innerHTML = `button:hover { background: #${color} }`;
-document.documentElement.append(style);
-
-let size = 24;
-const fixClockSize = () => {
-  clock.style.fontSize = size + 'pt';
-  const rect = clock.getBoundingClientRect();
-  ipc.send('resized', {
-    width: rect.width,
-    height: rect.height,
-  });
-};
-setTimeout(() => {
-  fixClockSize();
-}, 0);
-
-/**@type{any}*/(document).onmousewheel = (e) => {
-  const by = (e.deltaY > 0 ? -1 : 1) * 2;
-  size += by;
-  fixClockSize();
-};
-
-document.onmousedown = (e) => {
-  if (e.target === closeButton) return;
-  e.preventDefault();
-
-  document.onmousemove = (e) => {
-    e.preventDefault();
-    ipc.send('moved', e.movementX, e.movementY);
-  };
-};
 
 window.onblur = () => {
   closeButton.hidden = true;
@@ -49,21 +29,69 @@ window.onfocus = () => {
   }, 0);
 };
 
-document.onmouseup = (e) => {
-  document.onmousemove = null;
+function setupWindowsButtonColor() {
+  const color = new URLSearchParams(location.search).get('color');
+  const style = document.createElement('style');
+  style.nonce = 'clock';
+  style.innerHTML = `button:hover { background: #${color} }`;
+  document.documentElement.append(style);
+}
+
+
+// Drag to move
+
+document.onmousedown = (e) => {
+  if (e.target === closeButton) return;
+  e.preventDefault();
+
+  document.onmousemove = (e) => {
+    e.preventDefault();
+    window.moveBy(e.movementX, e.movementY);
+  };
+
+  document.onmouseup = (e) => {
+    e.preventDefault();
+    document.onmousemove = null;
+    document.onmouseup = null;
+  };
 };
 
-let seconds = false;
+
+// Scroll to resize
+
+document.onmousewheel = (e) => {
+  const by = (e.deltaY > 0 ? -1 : 1) * 2;
+  size += by;
+  size = Math.max(size, 11);
+  fixClockSize();
+};
+
+
+// Auto sizing
+
+function fixClockSize() {
+  clock.style.fontSize = size + 'pt';
+  const rect = clock.getBoundingClientRect();
+
+  const newWidth = Math.max(100, rect.width + 2);
+  const newHeight = Math.max(100, rect.height + 2);
+
+  const diffX = window.innerWidth - newWidth;
+  const diffY = window.innerHeight - newHeight;
+
+  window.resizeTo(newWidth, newHeight);
+  window.moveBy(diffX / 2, diffY / 2);
+}
+
+
+// Display
 
 clock.ondblclick = () => {
   seconds = !seconds;
   refreshClock();
-  fixClockSize();
 };
 
 function refreshClock() {
   clock.innerText = new Date().toLocaleTimeString([], { timeStyle: seconds ? 'medium' : 'short' });
+  fixClockSize();
 }
-
-refreshClock();
-setInterval(refreshClock, 1000);
--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const electron = require('electron');
-const path = require('path');
 
 electron.app.whenReady().then(() => {
   let win = new electron.BrowserWindow({
@@ -9,34 +8,6 @@ electron.app.whenReady().then(() => {
     backgroundColor: '#0a0a0a',
     frame: false,
     resizable: false,
-    webPreferences: {
-      // preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: true,
-    },
-  });
-
-  electron.ipcMain.on('moved', (e, x, y) => {
-    const [x1, y1] = win.getPosition();
-    win.setPosition(x1 + x, y1 + y);
-  });
-
-  electron.ipcMain.on('resized', (e, size) => {
-    if (size.width === 0 && size.height === 0) return;
-
-    const rect = win.getBounds();
-
-    const newWidth = Math.round(size.width * 1.5);
-    const newHeight = Math.round(size.height * 2.25);
-
-    const offsetX = Math.round((rect.width - newWidth) / 2);
-    const offsetY = Math.round((rect.height - newHeight) / 2);
-
-    rect.x += offsetX;
-    rect.y += offsetY;
-    rect.width = newWidth;
-    rect.height = newHeight;
-
-    win.setBounds(rect);
   });
 
   // win.webContents.toggleDevTools();
